d6522d17be50aeaba6cea281efaf3fc807b359af
androxxe <andriopratama16@gmail.com>
2023-09-08 14:47:49 +0700

    fix: fix Script tag inside Head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,7 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: { children: React.ReactNode; menu: string }) {
   return (
     <html lang="en">
-      <Head>
-        <Script async defer src="https://umami.andriosirait.com/script.js" data-website-id="366ca6bc-1e5d-4693-afb9-e737abcd2e41" data-domains="andriosirait.com"></Script>
-      </Head>
+      <Script async defer src="https://umami.andriosirait.com/script.js" data-website-id="366ca6bc-1e5d-4693-afb9-e737abcd2e41" data-domains="andriosirait.com"></Script>
       <body className={plusJakartaSans.className}>
         <main className="bg-background p-3 md:p-10 lg:h-screen">
           <div className="bg-gray-50 rounded-lg lg:h-full relative flex flex-col lg:flex-row justify-start items-start">
