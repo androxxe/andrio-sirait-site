23225902d468f84b30001c2cf9071325764c80e3
androxxe <andriopratama16@gmail.com>
2023-10-06 22:17:18 +0700

    chore: lower image quality for better performance

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -15,20 +15,14 @@ type PortfolioType = {
 
 const Portfolio = ({ portfolio }: PortfolioType) => {
   return (
-    <Link href={`/portfolio/${portfolio.slug}`} data-umami-event={`[PORTFOLIO][CARD] ${portfolio.name}`} >
+    <Link href={`/portfolio/${portfolio.slug}`} data-umami-event={`[PORTFOLIO][CARD] ${portfolio.name}`}>
       <div className="w-full h-80 relative border-2 border-slate-200 rounded-lg hover:scale-[101%] ease-out duration-100 group/portfolio">
         <div className="absolute top-0 right-0 z-10 mt-2 mx-2 flex flex-wrap gap-1 h-10 justify-end">
           {portfolio.tech_stack.map((tech_stack, index) => (
-            <div
-              key={index}
-              className="h-[22px] w-[30px] bg-white rounded shadow-sm border-[0.5px] border-slate-300 overflow-hidden relative"
-            >
+            <div key={index} className="h-[22px] w-[30px] bg-white rounded shadow-sm border-[0.5px] border-slate-300 overflow-hidden relative">
               <Image
-                src={
-                  technologies.find(
-                    (technology) => technology.code == tech_stack
-                  )?.logo_path || ""
-                }
+                quality={75}
+                src={technologies.find((technology) => technology.code == tech_stack)?.logo_path || ""}
                 fill={true}
                 sizes="100vw"
                 className="object-contain py-1 px-1"
@@ -39,23 +33,11 @@ const Portfolio = ({ portfolio }: PortfolioType) => {
         </div>
         <div className="invisible duration-200 rounded-lg ease-out group-hover/portfolio:visible absolute w-full h-full bg-slate-700 opacity-25"></div>
         <div className="w-full h-52 relative overflow-hidden">
-          <Image
-            className="object-cover"
-            src={portfolio.thumbnail}
-            fill={true}
-            quality={80}
-            loading="lazy"
-            alt={`Thumbnail ${portfolio.name}`}
-            sizes="100vw"
-          />
+          <Image className="object-cover" src={portfolio.thumbnail} fill={true} quality={80} loading="lazy" alt={`Thumbnail ${portfolio.name}`} sizes="100vw" />
         </div>
         <div className="p-3 h-44 duration-200 ease-in absolute w-full -bottom-16 left-0 right-0 group-hover/portfolio:-translate-y-16 group-hover/portfolio:rounded-t-xl group-hover/portfolio:rounded-b-lg group-hover/portfolio:bg-white">
-          <h6 className="font-bold text-primary-500 text-sm mb-1">
-            {portfolio.name}
-          </h6>
-          <p className="text-sm truncate group-hover/portfolio:whitespace-normal">
-            {portfolio.short_description}
-          </p>
+          <h6 className="font-bold text-primary-500 text-sm mb-1">{portfolio.name}</h6>
+          <p className="text-sm truncate group-hover/portfolio:whitespace-normal">{portfolio.short_description}</p>
         </div>
       </div>
     </Link>
--- src/components/SidebarProfile/index.tsx
+++ src/components/SidebarProfile/index.tsx
@@ -1,61 +1,39 @@
 import React from "react";
 import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { profile } from "@/data";
-import ImageProfile from "./ImageProfile";
+import Image from "next/image";
 
 const Profile = (): JSX.Element => {
   return (
     <div className="bg-primary-500 h-full w-full lg:w-2/5 rounded-tl-lg rounded-bl-lg rounded-tr-lg lg:rounded-tr-none relative">
-      <ImageProfile />
+      <div className="h-[350px] md:h-[420px] lg:h-full w-full relative">
+        <Image priority={true} className="object-contain" src={profile.photo_path} fill quality={40} alt={`Photo ${profile.name}`} content="fill" />
+      </div>
       <div className="absolute bottom-0 p-5 text-white text-center w-full bg-primary-600 rounded-bl-lg">
         <h2 className="font-bold">{profile.name}</h2>
         <h2 className="text-sm font-light">{profile.role}</h2>
         {Object.keys(profile.social_media).length > 0 ? (
           <div className="flex space-x-4 pt-5 flex-row justify-center items-center">
             {profile.social_media?.facebook ? (
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={profile.social_media.facebook}
-                className="text-lg"
-                data-umami-event="[SOCIAL MEDIA] Facebook"
-              >
+              <a target="_blank" rel="noreferrer" href={profile.social_media.facebook} className="text-lg" data-umami-event="[SOCIAL MEDIA] Facebook">
                 <FaFacebook />
               </a>
             ) : null}
 
             {profile.social_media?.instagram ? (
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={profile.social_media.instagram}
-                className="text-lg"
-                data-umami-event="[SOCIAL MEDIA] Instagram"
-              >
+              <a target="_blank" rel="noreferrer" href={profile.social_media.instagram} className="text-lg" data-umami-event="[SOCIAL MEDIA] Instagram">
                 <FaInstagram />
               </a>
             ) : null}
 
             {profile.social_media?.linked_in ? (
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={profile.social_media.linked_in}
-                className="text-lg"
-                data-umami-event="[SOCIAL MEDIA] Linked-In"
-              >
+              <a target="_blank" rel="noreferrer" href={profile.social_media.linked_in} className="text-lg" data-umami-event="[SOCIAL MEDIA] Linked-In">
                 <FaLinkedin />
               </a>
             ) : null}
 
             {profile.social_media?.github ? (
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={profile.social_media.github}
-                className="text-lg"
-                data-umami-event="[SOCIAL MEDIA] GitHub"
-              >
+              <a target="_blank" rel="noreferrer" href={profile.social_media.github} className="text-lg" data-umami-event="[SOCIAL MEDIA] GitHub">
                 <FaGithub />
               </a>
             ) : null}
