348ccacc25f57ad87068d0abf054ca04b99d16e5
Andrio Sirait <andriopratama16@gmail.com>
2026-01-13 23:46:04 +0700

    style: update whitespace in next.config.js and SidebarProfile component

--- src/components/SidebarProfile/index.tsx
+++ src/components/SidebarProfile/index.tsx
@@ -5,19 +5,20 @@ import Image from "next/image";
 
 const Profile = (): JSX.Element => {
   return (
-    <div className="bg-primary-500 h-full w-full lg:w-2/5 rounded-tl-lg rounded-bl-lg rounded-tr-lg lg:rounded-tr-none relative">
-      <div className="h-[350px] md:h-[420px] lg:h-full w-full relative">
-        <Image
-          priority={true}
-          className="object-contain"
-          src={profile.photo_path}
-          fill
-          quality={40}
-          alt={`Photo ${profile.name}`}
-          content="fill"
-        />
+    <div className="bg-neutral-500 h-full w-full lg:w-2/5 rounded-tl-lg rounded-bl-lg rounded-tr-lg lg:rounded-tr-none flex flex-col">
+      <div className="h-[240px] md:h-[420px] lg:flex-1 w-full px-8 pt-4 flex items-end overflow-hidden">
+        <div className="relative w-full h-full">
+          <Image
+            priority={true}
+            className="object-contain object-bottom"
+            src={profile.photo_path}
+            fill
+            quality={100}
+            alt={`Photo ${profile.name}`}
+          />
+        </div>
       </div>
-      <div className="absolute bottom-0 p-5 text-white text-center w-full bg-primary-600 rounded-bl-lg">
+      <div className="p-5 text-white text-center w-full bg-primary-600 rounded-bl-lg">
         <h2 className="font-bold">{profile.name}</h2>
         <h2 className="text-sm font-light">{profile.role}</h2>
         {Object.keys(profile.social_media).length > 0 ? (
