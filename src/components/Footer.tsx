7c3f852d92916ff909b81d257d03e226ed843d4b
Andrio Sirait <andriopratama16@gmail.com>
2026-01-13 22:44:57 +0700

    feat(footer, profile): update footer year dynamically and enhance profile description

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = (): JSX.Element => {
   return (
     <footer className="text-center items-center justify-center my-3">
       <div className="text-sm items-center justify-center text-white">
-        Andrio ©2023 crafted with ❤
+        Andrio © {new Date().getFullYear()} crafted with ❤
       </div>
     </footer>
   );
